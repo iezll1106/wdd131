07d018503f7858652332ae7579ba0e0f27879554
iezll1106 <llizardo@byupathway.edu>
2026-05-05 14:12:04 +0800

    add images, script amd css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get current year
-    document.getElementById('currentYear').innerText = new Date().getFullYear();
-  
-    // Get last modified date
-    document.getElementById('lastModified').innerText = 'Last Modified: ' + document.lastModified;
-  });  
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+document.getElementById("lastModified").textContent = document.lastModified;
