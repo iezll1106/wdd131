1840cc49de6e1142ff6d463bedaec57a6381d9b0
iezll1106 <llizardo@byupathway.edu>
2026-05-28 12:24:39 +0800

    edit filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -62,6 +62,7 @@ const temples = [
     imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
+// Add more temple objects here...
 
   {
     templeName: "Manila Philippines",
